c62a0e84cb5057adf8a997181c5876ebcb9ce73a
Paul McKellar <paul.mckellar@gmail.com>
2016-04-06 13:44:05 -0700

    static file server

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
+var connect = require('connect'),
+    serveStatic = require('serve-static');
 var http = require('http');
 
-http.createServer(onRequest).listen(3000);
+var app = connect();
+app.use(serveStatic("./"));
+http.createServer(app).listen(3000);
 
-function onRequest(client_req, client_res) {
-  console.log('serve: ' + client_req.url);
-  // console.log(client_req.url.split("/"))
 
-  if (client_req.url.split("/")[1] == "i") {
-    var options = {
-      hostname: 'hello-paulmckellar.revoreio.dev',
-      port: 80,
-      path: client_req.url,
-      method: 'GET'
-    };
+// http.createServer(onRequest).listen(3000);
 
-    var proxy = http.request(options, function (res) {
-      res.pipe(client_res, {
-        end: true
-      });
-    });
+// function onRequest(client_req, client_res) {
+//   console.log('serve: ' + client_req.url);
+//   // console.log(client_req.url.split("/"))
 
-    client_req.pipe(proxy, {
-      end: true
-    });
-  }
-  else {
+//   if (client_req.url.split("/")[1] == "i") {
+//     var options = {
+//       hostname: 'hello-paulmckellar.revoreio.dev',
+//       port: 80,
+//       path: client_req.url,
+//       method: 'GET'
+//     };
 
-  }
-}
+//     var proxy = http.request(options, function (res) {
+//       res.pipe(client_res, {
+//         end: true
+//       });
+//     });
+
+//     client_req.pipe(proxy, {
+//       end: true
+//     });
+//   }
+//   else {
+//     console.log("else");
+//     app.use(function(client_req, client_res){
+//       console.log("user");
+//       client_res.end('Hello from Connect!\n');
+//       console.log("ended");
+//     });
+//   }
+// }
