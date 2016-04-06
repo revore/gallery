a71305213b646a59ba49955addc8429f25a8d9ae
Paul McKellar <paul.mckellar@gmail.com>
2016-04-06 13:36:17 -0700

    server

--- server.js
+++ server.js
@@ -4,21 +4,27 @@ http.createServer(onRequest).listen(3000);
 
 function onRequest(client_req, client_res) {
   console.log('serve: ' + client_req.url);
+  // console.log(client_req.url.split("/"))
 
-  var options = {
-    hostname: 'www.revore.com',
-    port: 80,
-    path: client_req.url,
-    method: 'GET'
-  };
+  if (client_req.url.split("/")[1] == "i") {
+    var options = {
+      hostname: 'hello-paulmckellar.revoreio.dev',
+      port: 80,
+      path: client_req.url,
+      method: 'GET'
+    };
 
-  var proxy = http.request(options, function (res) {
-    res.pipe(client_res, {
+    var proxy = http.request(options, function (res) {
+      res.pipe(client_res, {
+        end: true
+      });
+    });
+
+    client_req.pipe(proxy, {
       end: true
     });
-  });
+  }
+  else {
 
-  client_req.pipe(proxy, {
-    end: true
-  });
+  }
 }
